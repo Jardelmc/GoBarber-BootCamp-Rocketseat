bac7cd427960f6a699f619c9da63094467f3e97d
Jardelmc <jardel.casteluber@gmail.com>
2019-06-29 15:39:05 -0300

    feature - Add user CRUD with password hash

---
+++ src/app/models/User.js
@@ -0,0 +1,27 @@
+import Sequelize, { Model } from 'sequelize';
+import bcrypt from 'bcryptjs';
+
+class User extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        name: Sequelize.STRING,
+        email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL, // Campo que nunca vai existir na base de dados.
+        password_hash: Sequelize.STRING,
+        provider: Sequelize.BOOLEAN,
+      },
+      {
+        sequelize,
+      }
+    );
+    this.addHook('beforeSave', async user => {
+      if (user.password) {
+        user.password_hash = await bcrypt.hash(user.password, 8);
+      }
+    }); // Trechos de código executados de forma automática baseado em ações que acontecem no model
+    return this;
+  }
+}
+
+export default User;
