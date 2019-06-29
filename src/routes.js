c4d8f286da7360918efc41b3b8a804903d807a5f
Jardelmc <jardel.casteluber@gmail.com>
2019-06-29 17:45:04 -0300

    chore - Authentication middleware to access

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,15 @@ import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
 routes.post('/session', SessionController.store);
+
+routes.use(authMiddleware); // Todas as rotas daqui para baixo só serão acessíveis se o usuário estiver logado
+
+routes.put('/users', UserController.update);
 
 export default routes;
