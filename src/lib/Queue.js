c47f076ded365e576fc5394cbe7f04e1e3e07efe
Jardelmc <jardel.casteluber@gmail.com>
2019-07-02 19:53:17 -0300

    error - Job method with error

---
+++ src/lib/Queue.js
@@ -0,0 +1,38 @@
+import Bee from 'bee-queue';
+import CancellationMail from '../app/jobs/CancellationMail';
+import redisConfig from '../config/redis';
+
+const jobs = [CancellationMail]; // Segue o modelo de importação dos models dentro de database/index
+
+class Queue {
+  constructor() {
+    this.queues = {};
+
+    this.init(); // Divide a parte de inicialização da fila em outro método
+  }
+
+  init() {
+    jobs.forEach(({ key, handle }) => {
+      this.queues[key] = {
+        bee: new Bee(key, {
+          redis: redisConfig,
+        }),
+        handle,
+      };
+    });
+  }
+
+  add(queue, job) {
+    return this.queues[queue].bee.createJob(job).save();
+  }
+
+  processQueue() {
+    jobs.forEach(job => {
+      const { bee, handle } = this.queues[job.key];
+
+      bee.process(handle);
+    });
+  }
+}
+
+export default new Queue();
