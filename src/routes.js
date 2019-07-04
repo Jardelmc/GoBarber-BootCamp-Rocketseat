3d4817d5d597c9db0be6abfc952fa04dba60ab70
Jardelmc <jardel.casteluber@gmail.com>
2019-07-03 23:04:19 -0300

    feature - Add index to list providers appointments avaliable or not

--- src/routes.js
+++ src/routes.js
@@ -11,6 +11,7 @@ import ScheduleController from './app/controllers/ScheduleController';
 import NotificationController from './app/controllers/NotificationController';
 
 import authMiddleware from './app/middlewares/auth';
+import AvaliableController from './app/controllers/AvaliableController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -23,6 +24,7 @@ routes.use(authMiddleware); // Todas as rotas daqui para baixo só serão acess�
 routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
+routes.get('/providers/:providerId/avaliable', AvaliableController.index);
 
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
