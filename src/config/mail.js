0b2e8ad3c9e3c6952f262bc82009af6ecee0f409
Jardelmc <jardel.casteluber@gmail.com>
2019-07-04 11:34:50 -0300

    feature - Add config to Environment Variables

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: 'c7e4f0602e1189',
-    pass: 'ea59e4e76a0cfe',
+    user: process.env.MAIL_HOST,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe GoBarber <noreply@gobarber.com>',
--- src/config/sentry.js
+++ src/config/sentry.js
@@ -1,3 +1,3 @@
 export default {
-  dsn: 'https://fccc3967192f4b8498dbfcfdc2790879@sentry.io/1496688',
+  dsn: process.env.SENTRY_DNS,
 };
